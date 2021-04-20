3b5bc4b4be4af44c79181bd5499e8ab717f5bd23
akshat-khosya <76739180+akshat-khosya@users.noreply.github.com>
2021-04-20 16:24:20 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+document.querySelector(".team").addEventListener("click", handleClick);
+function handleClick(){
+    alert("Team will be updated soon");
+}
+    
+document.querySelector(".teamb").addEventListener("click", handleClick);
+function handleClick(){
+    alert("Team will be updated soon");
+}
+  
