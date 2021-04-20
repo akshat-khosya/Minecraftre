60c56504aabc091674b2d974dd55fb985fb1e60d
akshat-khosya <76739180+akshat-khosya@users.noreply.github.com>
2021-04-20 20:45:13 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -3,11 +3,15 @@ function handleClick(){
     alert("Team will be updated soon");
 }
     
-document.querySelector(".teamb").addEventListener("click", handleClick);
-function handleClick(){
+document.querySelector(".teamb").addEventListener("click", teambclick);
+function teambclick(){
     alert("Team will be updated soon");
 }
-document.querySelector(".event").addEventListener("click", handleClick);
-function handleClick(){
+document.querySelector(".event").addEventListener("click", eventclick);
+function eventclick(){
     alert("No Event Yet!");
+}
+document.querySelector(".clickme").addEventListener("click", meclick);
+function meclick(){
+    alert("This Section will be updated soon");
 }
