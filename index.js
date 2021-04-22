a08e618b1887c73b2f183184c499d00ee201c2da
akshat-khosya <76739180+akshat-khosya@users.noreply.github.com>
2021-04-22 12:09:17 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,46 +1,76 @@
 document.querySelector(".team").addEventListener("click", handleClick);
-function handleClick(){
+function handleClick() {
     alert("Team will be updated soon");
 }
-    
+
 document.querySelector(".teamb").addEventListener("click", teambclick);
-function teambclick(){
+function teambclick() {
     alert("Team will be updated soon");
 }
 document.querySelector(".event").addEventListener("click", eventclick);
-function eventclick(){
+function eventclick() {
     alert("No Event Yet!");
 }
 document.querySelector(".clickme").addEventListener("click", meclick);
-function meclick(){
+function meclick() {
     alert("This Section will be updated soon");
 }
 document.querySelector(".navbar-brand").addEventListener("click", brandclick);
-function brandclick(){
-    var numcolor=Math.round(Math.random()*7);
-    if(numcolor===0){
-        document.querySelector(".navbar-brand").style.color="red";
+function brandclick() {
+    var numcolor = Math.round(Math.random() * 7);
+    if (numcolor === 0) {
+        document.querySelector(".navbar-brand").style.color = "red";
     }
-    else if(numcolor===1){
-        document.querySelector(".navbar-brand").style.color="green";
+    else if (numcolor === 1) {
+        document.querySelector(".navbar-brand").style.color = "green";
     }
-    else if(numcolor===2){
-        document.querySelector(".navbar-brand").style.color="white";
+    else if (numcolor === 2) {
+        document.querySelector(".navbar-brand").style.color = "white";
     }
-    else if(numcolor===3){
-        document.querySelector(".navbar-brand").style.color="blue";
+    else if (numcolor === 3) {
+        document.querySelector(".navbar-brand").style.color = "blue";
     }
-    else if(numcolor===4){
-        document.querySelector(".navbar-brand").style.color="yellow";
+    else if (numcolor === 4) {
+        document.querySelector(".navbar-brand").style.color = "yellow";
     }
-    else if(numcolor===5){
-        document.querySelector(".navbar-brand").style.color="pink";
+    else if (numcolor === 5) {
+        document.querySelector(".navbar-brand").style.color = "pink";
     }
-    else if(numcolor===6){
-        document.querySelector(".navbar-brand").style.color="brown";
+    else if (numcolor === 6) {
+        document.querySelector(".navbar-brand").style.color = "brown";
     }
 }
 
+// document.querySelectorAll(".nav-link").addEventListener("click", navitemclick);
+// function navitemclick(){
+//     for(var i=0; i<3;i++){
+//     var numitemcolor=Math.round(Math.random()*7);
+
+//     if(numitemcolor===0){
+//         document.querySelectorAll(".nav-link")[i].style.color="red";
+//     }
+//     else if(numitemcolor===1){
+//         document.querySelectorAll(".nav-link")[i].style.color="green";
+//     }
+//     else if(numitemcolor===2){
+//         document.querySelectorAll(".nav-link")[i].style.color="white";
+//     }
+//     else if(numitemcolor===3){
+//         document.querySelectorAll(".nav-link")[i].style.color="blue";
+//     }
+//     else if(numitemcolor===4){
+//         document.querySelectorAll(".nav-link")[i].style.color="yellow";
+//     }
+//     else if(numitemcolor===5){
+//         document.querySelectorAll(".nav-link")[i].style.color="pink";
+//     }
+//     else if(numitemcolor===6){
+//         document.querySelectorAll(".nav-link")[i].style.color="brown";
+//     }
+
+
+// }
+// }
 var navlinknum = document.querySelectorAll(".navbaritem").length;
 
    for(var i=0;i<navlinknum;i++){
@@ -130,14 +160,17 @@ var navlinknum = document.querySelectorAll(".navbaritem").length;
 } 
 
    
+    
 document.querySelector(".why-img").addEventListener("mouseenter", imgeffect);
 function imgeffect() {
     document.querySelector(".why-img").src = "Image/what.jpg";
     document.querySelector(".why-img").style.width="102%";
+    document.querySelector(".why-img").style.height="93%";
 }
 document.querySelector(".why-img").addEventListener("mouseleave", imgeffectrev);
 function imgeffectrev(){
     document.querySelector(".why-img").src = "Image/what-black.jpg";
     document.querySelector(".why-img").style.width="100%";
+    document.querySelector(".why-img").style.height="90%";
     
 }
